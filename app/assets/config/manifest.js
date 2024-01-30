42b5c7c6642cc6db3e84355769c7ed7f4443e494
Joshua Rummell <jjrummell5@tamu.edu>
2024-01-29 23:01:43 -0600

    Initial Book App

---
+++ app/assets/config/manifest.js
@@ -0,0 +1,6 @@
+//= link application.js
+//= link controllers/application.js
+//= link controllers/hello_controller.js
+//= link controllers/index.js
+//= link_tree ../images
+//= link_directory ../stylesheets .css
